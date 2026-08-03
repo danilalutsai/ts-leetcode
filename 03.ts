0390d383499d5ad2133cdf03137e5102e32604ce
Danila Lutsai <dlutsai98@gmail.com>
2026-08-03 21:45:52 +0300

    args length

---
+++ 03.ts
@@ -0,0 +1,9 @@
+// Return length of arguments passed
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+  return args.length;
+}
+
+argumentsLength({}, null, '3');
