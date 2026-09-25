b97a39217e2f43b5e50a5878ddf60155692bb543
Danila Lutsai <dlutsai98@gmail.com>
2026-09-25 18:58:20 +0300

    exercises

--- 38.ts
+++ 38.ts
@@ -18,7 +18,6 @@ function searchInsert(nums: number[], target: number): number {
   return 0;
 }
 
-
 console.log(searchInsert([1,3,5], 4));
 
 function searchInsertBinary(nums: number[], target: number): number {
